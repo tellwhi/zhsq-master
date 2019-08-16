a9d2e2e3f6f17310c558446d4216fb719a7c8b5c
monster <1093846898@qq.com>
2019-08-16 17:21:24 +0800

    cookieStatusWithCookie

--- src/main/java/com/monster/zhaqsq/utils/cookieUtils.java
+++ src/main/java/com/monster/zhaqsq/utils/cookieUtils.java
@@ -5,6 +5,7 @@ import com.monster.zhaqsq.utils.encryptUtils;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /**
@@ -16,6 +17,9 @@ public class cookieUtils {
     static byte[] key = {-22, -14, 74, -23, -29, 94, 103, 47};
     static String UserId;
     static String UserType;
+
+    static long adminKeepTime = 86400000;
+    static long userKeepTime = 604800000;
 
 
     /**
@@ -100,14 +104,54 @@ public class cookieUtils {
     }
 
 
-
+    /**
+     * 清除用户cookie
+     * @param request
+     * @param response
+     */
     public static void clearCookie(HttpServletRequest request,HttpServletResponse response){
         Cookie[] cookies=request.getCookies();
-
         for(Cookie cookie: cookies){
             cookie.setMaxAge(0);
             cookie.setPath("/");
             response.addCookie(cookie);
+        }
+    }
+
+    /**
+     * 管理员cookie有效期一天
+     * @param request 管理员HttpRequest
+     * @return 登陆状态是否有效
+     * @throws Exception 取不得到时间
+     */
+    public static Boolean adminLoginTimeStatus(HttpServletRequest request) throws Exception{
+        Date date= new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date loginTime = sdf.parse(getTime(request));
+        if(date.getTime() >= loginTime.getTime() + adminKeepTime){
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+
+
+    /**
+     * 普通用户登陆cookie有效期7天
+     * @param request 用户HttpRequest
+     * @return 登陆状态是否有效
+     * @throws Exception 取不得到时间
+     */
+    public static Boolean userLoginTimeStatus(HttpServletRequest request) throws Exception{
+        Date date= new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date loginTime = sdf.parse(getTime(request));
+        if(date.getTime() >= loginTime.getTime() + userKeepTime){
+            return false;
+        }
+        else {
+            return true;
         }
     }
 
